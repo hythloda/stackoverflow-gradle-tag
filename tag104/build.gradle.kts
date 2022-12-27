f6ece9e42825912b3daacfc160d4b60fef20c8b8
Amanda Martin <amartin@gradle.com>
2022-12-27 10:25:30 -0500

    More tags

--- tag104/build.gradle.kts
+++ tag104/build.gradle.kts
@@ -3,3 +3,21 @@ jar {
         attributes["Main-Class"] = "com.foo.bar.MainClass"
     }
 }
+
+
+//Boris's answer
+tasks {
+    register("standaloneJar", Jar::class.java) {
+        archiveClassifier.set("all")
+        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+        manifest {
+            attributes["Main-Class"] = "com.google.android.gradle_recipe.converter.ConvertToolKt"
+        }
+        from(configurations.runtimeClasspath.get()
+            .onEach { println("add from dependencies: ${it.name}") }
+            .map { if (it.isDirectory) it else zipTree(it) })
+        val sourcesMain = sourceSets.main.get()
+        sourcesMain.allSource.forEach { println("add from sources: ${it.name}") }
+        from(sourcesMain.output)
+    }
+}
--- tag50/build.gradle.kts
+++ tag50/build.gradle.kts
@@ -1,3 +1,17 @@
 tasks.register<Wrapper>("wrapper") {
     gradleVersion = "4.1"
 }
+
+//not sure if this works
+tasks {
+    wrapper("wrapper", Wrapper::class)
+    gradleVersion = "4.1"
+}
+
+//not sure if this is better
+tasks {
+    wrapper("wrapper") {
+        type = Wrapper::class
+        gradleVersion = "4.1"
+    }
+}
